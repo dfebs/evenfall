a51a5b04fb36f57c5967c6f27a0a8314d7727fc4
dfebs <daniel.febles97@gmail.com>
2025-07-23 18:23:16 -0400

    Add index to User model

--- db/migrate/20250723221234_create_users.rb
+++ db/migrate/20250723221234_create_users.rb
@@ -3,9 +3,11 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
+      t.string :username, null: false
 
       t.timestamps
     end
     add_index :users, :email_address, unique: true
+    add_index :users, :username, unique: true
   end
 end
