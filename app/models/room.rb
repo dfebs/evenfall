249fd2e692f4656d681b24b3ef07423d44fd47cd
dfebs <daniel.febles97@gmail.com>
2025-07-23 18:36:39 -0400

    Wire up User and Room models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
+  has_many :messages, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :memberships, dependent: :destroy
+  has_many :rooms, through: :memberships
+  has_many :messages, dependent: :destroy
+
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
