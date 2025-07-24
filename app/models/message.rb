61e7557c4d0b5ef08ba8448758d49dbadeec7bc1
dfebs <daniel.febles97@gmail.com>
2025-07-24 19:46:32 -0400

    Add real time messaging

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   has_rich_text :content
+
+  after_create_commit -> { broadcast_append_to "room_messages_sub_#{self.room.id}", partial: "messages/message", locals: { room: room, message: self }, target: "messages" }
 end
