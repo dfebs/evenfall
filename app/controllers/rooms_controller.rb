3b9f902b5face20ed221e4e5d7c35ba50bda6208
dfebs <daniel.febles97@gmail.com>
2025-07-24 19:45:53 -0400

    Add ability to create rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,36 @@ class RoomsController < ApplicationController
   def show
   end
 
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @membership = Membership.new(room: @room, user: Current.user, role: "owner")
+
+    begin
+      ActiveRecord::Base.transaction do
+        @room.save!
+        @membership.save!
+      end
+
+      respond_to do | format |
+        format.html { redirect_to @room, notice: "Room successfully created" }
+      end
+
+    rescue ActiveRecord::RecordInvalid
+      respond_to do | format |
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def room_params
+    params.expect room: [ :name ]
+  end
 
   def set_room
     @room = Room.find(params.expect(:id))
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@ class Room < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
   has_many :messages, dependent: :destroy
+
+  def owner
+    self.memberships.find_by(role: "owner")&.user
+  end
 end
