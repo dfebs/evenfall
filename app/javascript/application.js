000e2e97525fad4b97d1af9a68f2f47f7646e232
dfebs <daniel.febles97@gmail.com>
2025-07-27 12:13:41 -0400

    Allow users to add newline characters when holding shift

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,20 @@ import "trix"
 import "@rails/actiontext"
 
 const element = document.querySelector("trix-editor")
+// TODO make the #messages selector scroll down when something is added to it, or when form is submitted
+let isShiftHeld = false;
+
+window.addEventListener ("keydown", event => {
+  if (event.key === "Shift") isShiftHeld = true;
+});
+
+window.addEventListener ("keyup", event => {
+  if (event.key === "Shift") isShiftHeld = false;
+});
 
 element.addEventListener("keydown", event => {
-  if (event.key == "Enter") {
-    event.preventDefault()
-    element.inputElement.form.submit()
+  if (event.key == "Enter" && !isShiftHeld) {
+    event.preventDefault();
+    element.inputElement.form.submit();
   }
 });
